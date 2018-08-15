b7d8c43f51803870f6f2ae1fb71b72e94586c645
AdnoC <adam.r.cutler@gmail.com>
2018-08-15 19:44:29 -0400

    HMTX::metric_for_glyph

--- src/tables/hmtx.rs
+++ src/tables/hmtx.rs
@@ -22,10 +22,20 @@ impl<'a> HMTX<'a> {
             DynArr::<LongHorizMetric>::split_buf_for_len(buf, num_horiz_metrics as usize);
         let horiz_metrics = DynArr(horiz_metric_buf, PhantomData);
         let left_bearings = DynArr(left_bearings_buf, PhantomData);
+        // assert_eq: left_bearings.len(), num_glyphs - num_horiz_metrics
 
         HMTX {
             horiz_metrics,
             left_bearings,
+        }
+    }
+
+    pub fn metric_for_glyph(&self, glyph_id: usize) -> HorizMetric {
+        if glyph_id < self.horiz_metrics.len() {
+            self.horiz_metrics.at(glyph_id).into()
+        } else {
+            let idx = glyph_id - self.horiz_metrics.len();
+            self.left_bearings.at(idx).into()
         }
     }
 }
@@ -34,4 +44,27 @@ impl<'a> HMTX<'a> {
 pub struct LongHorizMetric {
     advance_width: u16,
     left_bearing: i16,
+}
+
+#[derive(Debug)]
+pub struct HorizMetric {
+    advance_width: Option<u16>,
+    left_bearing: i16,
+}
+
+impl From<i16> for HorizMetric {
+    fn from(left_bearing: i16) -> Self {
+        HorizMetric {
+            advance_width: None,
+            left_bearing,
+        }
+    }
+}
+impl From<LongHorizMetric> for HorizMetric {
+    fn from(LongHorizMetric {  left_bearing, advance_width }: LongHorizMetric) -> Self {
+        HorizMetric {
+            advance_width: Some(advance_width),
+            left_bearing,
+        }
+    }
 }
